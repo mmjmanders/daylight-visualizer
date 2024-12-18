3acfb574eb277b6d7cba9ce0927b47ebe7bcca80
Mark Manders <mmjmanders@gmail.com>
2024-12-18 07:50:59 +0100

    WIP

--- src/app/components/coordinates-form/CoordinatesForm.vue
+++ src/app/components/coordinates-form/CoordinatesForm.vue
@@ -3,7 +3,7 @@ import { useForm } from 'vee-validate';
 import { toTypedSchema } from '@vee-validate/zod';
 import { number, object } from 'zod';
 
-const { defineField, handleSubmit, errors, setFieldValue } = useForm({
+const { meta, defineField, handleSubmit, errors, setFieldValue } = useForm({
   validationSchema: toTypedSchema(
     object({
       latitude: number().min(-90).max(90),
@@ -25,32 +25,26 @@ const onSubmit = handleSubmit(async ({ latitude, longitude }) => {
 </script>
 
 <template>
-  <form
-    class="flex items-end gap-2"
-    novalidate
-    @submit.prevent="onSubmit"
-  >
+  <form class="flex items-end gap-2" novalidate @submit.prevent="onSubmit">
     <label class="block">
-      <span class="block">Latitude</span>
+      <span class="block mb-1">Latitude</span>
       <input
         v-model="latitude"
+        :class="{ error: errors.latitude }"
         type="number"
         v-bind="latitudeAttrs"
-      >
+      />
     </label>
     <label class="block">
-      <span class="block">Longitude</span>
+      <span class="block mb-1">Longitude</span>
       <input
         v-model="longitude"
+        :class="{ error: errors.longitude }"
         type="number"
         v-bind="longitudeAttrs"
-      >
+      />
     </label>
-    <button
-      class="align-bottom"
-      type="submit"
-      :disabled="Object.keys(errors).length > 0"
-    >
+    <button class="align-bottom" type="submit" :disabled="!meta.valid">
       Submit
     </button>
   </form>
