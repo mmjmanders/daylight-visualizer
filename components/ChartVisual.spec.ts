13d129bce93a4173774f241b8efc1cf5de6c0543
Mark Manders <mmjmanders@gmail.com>
2025-01-23 11:38:45 +0100

    Updated favicon to match colors of chart

---
+++ components/ChartVisual.spec.ts
@@ -0,0 +1,20 @@
+import { beforeEach, describe, expect, it } from 'vitest';
+import { mountSuspended } from '@nuxt/test-utils/runtime';
+import type { VueWrapper } from '@vue/test-utils';
+import { ChartVisual } from '#components';
+
+describe('ChartVisual', () => {
+  let component: VueWrapper;
+
+  beforeEach(async () => {
+    component = await mountSuspended(ChartVisual, {
+      props: {
+        data: [],
+      },
+    });
+  });
+
+  it('should render', async () => {
+    expect(component.find('.highcharts-light')).toBeTruthy();
+  });
+});
