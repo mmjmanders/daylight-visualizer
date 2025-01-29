8f8c1340c01fcb68419cda20065df4a94f45e97c
Mark Manders <mmjmanders@gmail.com>
2025-01-29 11:41:19 +0100

    Add device-specific projects to Playwright config (#24)
    * Add device-specific projects to Playwright config
    
    Enhanced the Playwright configuration to include testing projects for various devices and browsers, such as Chrome, Safari, Firefox, and mobile devices like iPhone and Galaxy S5. This ensures better test coverage across multiple environments.
    
    * Add HOME environment variable to e2e test step
    
    The HOME variable is set to ensure consistent behavior during e2e tests. This change addresses potential issues with certain tools or dependencies relying on the HOME path.
    
    * Enable parallel test execution and adjust global timeout
    
    Set test.describe to run in parallel mode for e2e tests to improve execution speed. Removed unnecessary "fullyParallel" config and simplified globalTimeout expression for CI environments.
    
    * Refactor submit button tests for dynamic input scenarios
    
    Replaced multiple static tests with a parameterized approach to test submit button behavior for various input combinations. This improves code readability, reduces duplication, and ensures better scalability for future test cases.
    
    * Add geolocation support and test for location-based data
    
    Configured geolocation permissions and default coordinates in Playwright settings. Added a new end-to-end test to verify form population using the geolocation API. This enhances test coverage for location-dependent features.

--- tests/e2e/chart.spec.ts
+++ tests/e2e/chart.spec.ts
@@ -11,16 +11,31 @@ test.describe('Chart', () => {
     expect(await submitButton.isDisabled()).toBe(true);
   });
 
-  test('should have submit button enabled on valid form', async ({ page }) => {
-    await page.locator('#latitude').fill('51.0132');
-    await page.locator('#longitude').fill('13.7384');
-    await page.locator('#startDate').fill('2025-01-01');
-    await page.locator('#endDate').fill('2025-01-27');
-    const submitButton = page.locator('button[type=submit]');
-    await page.waitForFunction(() =>
-      !(document.querySelector('button[type=submit]') as HTMLButtonElement).disabled,
-    );
-    expect(await submitButton.isEnabled()).toBe(true);
+  [{
+    latitude: '51.0132',
+    longitude: '13.7384',
+    startDate: '2025-01-01',
+    endDate: '2025-01-27',
+    submitEnabled: true,
+  }, {
+    latitude: '51.0132',
+    longitude: '13.7384',
+    startDate: '2024-01-01',
+    endDate: '2025-01-27',
+    submitEnabled: false,
+  }].forEach(({ latitude, longitude, startDate, endDate, submitEnabled }) => {
+    test(`should have submit button ${submitEnabled ? 'en' : 'dis'}abled for input values latitude: ${latitude}, longitude: ${longitude}, startDate: ${startDate}, endDate: ${endDate} `, async ({ page }) => {
+      await page.locator('#latitude').fill(latitude);
+      await page.locator('#longitude').fill(longitude);
+      await page.locator('#startDate').fill(startDate);
+      await page.locator('#endDate').fill(endDate);
+      const submitButton = page.locator('button[type=submit]');
+      await page.waitForFunction(
+        (expectedState: boolean) =>
+          (document.querySelector('button[type=submit]') as HTMLButtonElement).disabled === expectedState, !submitEnabled,
+      );
+      expect(await submitButton.isEnabled()).toBe(submitEnabled);
+    });
   });
 
   test('should show chart on valid data', async ({ page }) => {
@@ -39,15 +54,12 @@ test.describe('Chart', () => {
     expect(page.locator('div.highcharts-light')).toBeDefined();
   });
 
-  test('should have submit button disabled on invalid form', async ({ page }) => {
-    await page.locator('#latitude').fill('51.0132');
-    await page.locator('#longitude').fill('13.7384');
-    await page.locator('#startDate').fill('2024-01-01');
-    await page.locator('#endDate').fill('2025-01-27');
-    const submitButton = page.locator('button[type=submit]');
+  test('should fill data with geolocation api', async ({ page }) => {
+    await page.locator('button[type=button]').click();
     await page.waitForFunction(() =>
-      (document.querySelector('button[type=submit]') as HTMLButtonElement).disabled,
+      !(document.querySelector('button[type=submit]') as HTMLButtonElement).disabled,
     );
-    expect(await submitButton.isDisabled()).toBe(true);
+    expect(await page.locator('#latitude').inputValue()).toBe('41.889938');
+    expect(await page.locator('#longitude').inputValue()).toBe('12.492507');
   });
 });
