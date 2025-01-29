a75f684721f85f0050eb5442c5b5d62da19ee258
Mark Manders <mmjmanders@gmail.com>
2025-01-29 09:10:02 +0100

    Add device-specific projects to Playwright config
    Enhanced the Playwright configuration to include testing projects for various devices and browsers, such as Chrome, Safari, Firefox, and mobile devices like iPhone and Galaxy S5. This ensures better test coverage across multiple environments.

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,9 +1,55 @@
 import { fileURLToPath } from 'node:url';
-import { defineConfig } from '@playwright/test';
+import { defineConfig, devices } from '@playwright/test';
 import type { ConfigOptions } from '@nuxt/test-utils/playwright';
 
 export default defineConfig<ConfigOptions>({
+  fullyParallel: true,
+  globalTimeout: process.env.CI ? (60 * 60 * 1000) : undefined,
   testDir: 'tests/e2e',
+  projects: [
+    {
+      name: 'Desktop Chrome',
+      use: {
+        ...devices['Desktop Chrome'],
+      },
+    }, {
+      name: 'Desktop Edge',
+      use: {
+        ...devices['Desktop Edge'],
+      },
+    }, {
+      name: 'Desktop Firefox',
+      use: {
+        ...devices['Desktop Firefox'],
+      },
+    }, {
+      name: 'Desktop Safari',
+      use: {
+        ...devices['Desktop Safari'],
+      },
+    }, {
+      name: 'iPhone Safari',
+      use: {
+        ...devices['iPhone 11'],
+      },
+    }, {
+      name: 'iPad Safari',
+      use: {
+        ...devices['iPad (gen 6)'],
+      },
+    }, {
+      name: 'Galaxy S5',
+      use: {
+        ...devices['Galaxy S5'],
+      },
+    },
+    {
+      name: 'Pixel 7',
+      use: {
+        ...devices['Pixel 7'],
+      },
+    },
+  ],
   use: {
     nuxt: {
       rootDir: fileURLToPath(new URL('.', import.meta.url)),
