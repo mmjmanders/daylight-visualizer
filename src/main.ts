4a82b4e1b943da3cdb4c6c8c166a97c40eb202ad
Mark Manders <mmjmanders@gmail.com>
2025-02-26 10:28:09 +0100

    Add libraries and favicons (#2)
    * Add favicon.svg to the public directory
    
    Introduce a new favicon for the project by adding favicon.svg to the public directory. This enhances the site's branding and improves user experience by displaying a recognizable icon in browser tabs.
    
    * Add pre-commit hook and update dependencies
    
    Introduced a new pre-commit hook using Husky and lint-staged for automatic linting before commits. Updated `pnpm-lock.yaml` to include new dependencies such as Husky, lint-staged, and other necessary packages to support this functionality.
    
    * Update pnpm-lock.yaml with new dependencies and versions
    
    Added multiple new dependencies like FontAwesome, Bulma, Highcharts, Luxon, and Vue I18n to the lockfile. Updated or replaced several existing dependencies and their versions for better compatibility and functionality. Removed unused or outdated dependencies, ensuring consistency and up-to-date library references.
    
    * Add font source packages and optimize build output
    
    Added '@fontsource/fira-mono', '@fontsource/oxygen', and '@fontsource/palanquin' for custom fonts with appropriate imports in `main.ts`. Updated `vite.config.ts` to include manual chunk splitting for better build output. Changed test environment in `vitest.config.ts` from 'jsdom' to 'happy-dom' for improved compatibility.
    
    * Add VueQueryDevtools and .env example file
    
    Updated the application to include VueQueryDevtools for debugging Vue Query. Added a `.env.example` file to provide base environment variable configurations, and updated `.gitignore` to exclude `.env` files.
    
    * Enable testing on mobile and branded browser configurations.
    
    Uncommented configurations for Mobile Chrome, Mobile Safari, Microsoft Edge, and Google Chrome in the Playwright setup. This ensures the tests now cover additional devices and browser channels for better compatibility validation.
    
    * Disable testing against Edge and Chrome in Playwright.
    
    Commented out configurations for Microsoft Edge and Google Chrome testing. This change may help streamline the test setup or focus testing on other browser environments.
    
    * Add vee-validate and yup dependencies
    
    Introduced vee-validate and @vee-validate/yup for form validation, along with yup for schema definitions. Updated the pnpm-lock.yaml and package.json to include these new dependencies.

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,30 @@
+import '@fontsource/palanquin/latin-600.css'
+import '@fontsource/palanquin/latin-700.css'
+import '@fontsource/oxygen/latin-400.css'
+import '@fontsource/oxygen/latin-700.css'
+import '@fontsource/fira-mono/400.css'
+
 import './assets/main.css'
 
+import HighchartsVue from 'highcharts-vue'
+import { VueQueryPlugin } from '@tanstack/vue-query'
+import * as en from './locales/en.json'
+import * as nl from './locales/nl.json'
+import { createI18n } from 'vue-i18n'
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+createApp(App)
+  .use(HighchartsVue)
+  .use(VueQueryPlugin)
+  .use(
+    createI18n({
+      legacy: false,
+      fallbackLocale: 'en',
+      messages: {
+        en,
+        nl,
+      },
+    }),
+  )
+  .mount('#app')
