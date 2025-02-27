9c99c6a540abdf00f2b77a94fc97ba942f32df9e
Mark Manders <mmjmanders@gmail.com>
2025-02-27 10:20:12 +0100

    Add @tanstack/eslint-plugin-query and update ESLint config
    Integrated `@tanstack/eslint-plugin-query` with the project by adding it to `devDependencies` and ESLint configuration. Updated ignored file patterns to include test-related folders. Also updated `pnpm` version specified in `package.json`.

--- eslint.config.ts
+++ eslint.config.ts
@@ -3,6 +3,7 @@ import { defineConfigWithVueTs, vueTsConfigs } from '@vue/eslint-config-typescri
 import pluginVitest from '@vitest/eslint-plugin'
 import pluginPlaywright from 'eslint-plugin-playwright'
 import skipFormatting from '@vue/eslint-config-prettier/skip-formatting'
+import pluginQuery from '@tanstack/eslint-plugin-query'
 
 // To allow more languages other than `ts` in `.vue` files, uncomment the following lines:
 // import { configureVueProject } from '@vue/eslint-config-typescript'
@@ -17,20 +18,28 @@ export default defineConfigWithVueTs(
 
   {
     name: 'app/files-to-ignore',
-    ignores: ['**/dist/**', '**/dist-ssr/**', '**/coverage/**'],
+    ignores: [
+      '**/dist/**',
+      '**/dist-ssr/**',
+      '**/coverage/**',
+      '**/playwright-report/**',
+      '**/test-results/**',
+    ],
   },
 
   pluginVue.configs['flat/essential'],
+  pluginQuery.configs['flat/recommended'],
   vueTsConfigs.recommended,
-  
+
   {
     ...pluginVitest.configs.recommended,
     files: ['src/**/__tests__/*'],
   },
-  
+
   {
     ...pluginPlaywright.configs['flat/recommended'],
     files: ['e2e/**/*.{test,spec}.{js,ts,jsx,tsx}'],
   },
+
   skipFormatting,
 )
