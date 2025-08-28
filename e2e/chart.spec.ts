500782ab8e6c368742203e5230bc4f434de6fc66
Mark Manders <mmjmanders@gmail.com>
2025-08-28 13:10:14 +0200

    Grouped other tests

--- e2e/chart.spec.ts
+++ e2e/chart.spec.ts
@@ -1,25 +1,29 @@
 import { test, expect } from '@playwright/test';
 
-test.use({
-  permissions: ['geolocation'],
-  geolocation: { latitude: 40.71427, longitude: -74.00597 },
-});
+test.describe('Chart', () => {
+  test.use({
+    permissions: ['geolocation'],
+    geolocation: { latitude: 40.71427, longitude: -74.00597 },
+  });
 
-test('show chart on valid data', async ({ page }) => {
-  await page.goto('/');
-  await page.locator('button[type=button]').click();
-  await page.locator('button[type=submit]').click();
-  await page.locator('div.highcharts-container').waitFor();
-  await expect(page.locator('div.highcharts-container')).toBeVisible();
-});
+  test.beforeEach(async ({ page }) => {
+    await page.goto('/');
+  });
 
-test('should switch chart type and css class', async ({ page, viewport }) => {
-  test.skip(viewport != null && viewport.width < 992, 'Test only runs on wide screens');
-  await page.goto('/');
-  await page.locator('button[type=button]').click();
-  await page.locator('button[type=submit]').click();
-  await page.locator('div.highcharts-container').waitFor();
-  await expect(page.locator('div.chart-container.polar')).toBeVisible();
-  await page.locator('#chartType').selectOption('line');
-  await expect(page.locator('div.chart-container.line')).toBeVisible();
+  test('show chart on valid data', async ({ page }) => {
+    await page.locator('button[type=button]').click();
+    await page.locator('button[type=submit]').click();
+    await page.locator('div.highcharts-container').waitFor();
+    await expect(page.locator('div.highcharts-container')).toBeVisible();
+  });
+
+  test('should switch chart type and css class', async ({ page, viewport }) => {
+    test.skip(viewport != null && viewport.width < 992, 'Test only runs on wide screens');
+    await page.locator('button[type=button]').click();
+    await page.locator('button[type=submit]').click();
+    await page.locator('div.highcharts-container').waitFor();
+    await expect(page.locator('div.chart-container.polar')).toBeVisible();
+    await page.locator('#chartType').selectOption('line');
+    await expect(page.locator('div.chart-container.line')).toBeVisible();
+  });
 });
