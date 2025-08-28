1f6122e8732c5e84973aa9715c27f7a1f3c6070a
Mark Manders <mmjmanders@gmail.com>
2025-08-28 13:02:56 +0200

    Grouped App tests

--- e2e/app.spec.ts
+++ e2e/app.spec.ts
@@ -2,32 +2,36 @@ import { expect, test } from '@playwright/test';
 
 const buildSha = process.env.CI ? 'e2e' : 'dev';
 
-test('header should contain "Daylight Visualizer"', async ({ page }) => {
-  await page.goto('/');
-  await expect(page.locator('h1')).toContainText('Daylight Visualizer');
-});
+test.describe('App', () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto('/');
+  });
 
-test(`version should be "${buildSha}"`, async ({ page }) => {
-  await page.goto('/');
-  await expect(page.locator('h1 > span')).toContainText(`- ${buildSha}`);
-});
+  test('header should contain "Daylight Visualizer"', async ({ page }) => {
+    await expect(page.locator('h1')).toContainText('Daylight Visualizer');
+  });
 
-[
-  {
-    lang: 'nl',
-    submitText: 'Verzenden',
-  },
-  {
-    lang: 'en',
-    submitText: 'Submit',
-  },
-  {
-    lang: 'fr',
-    submitText: 'Submit',
-  },
-].forEach(({ lang, submitText }) => {
-  test(`language ${lang} should have ${submitText} as button text`, async ({ page }) => {
-    await page.goto(`/?lang=${lang}`);
-    await expect(page.locator('button[type=submit]')).toHaveText(submitText);
+  test(`version should be "${buildSha}"`, async ({ page }) => {
+    await expect(page.locator('h1 > span')).toContainText(`- ${buildSha}`);
+  });
+
+  [
+    {
+      lang: 'nl',
+      submitText: 'Verzenden',
+    },
+    {
+      lang: 'en',
+      submitText: 'Submit',
+    },
+    {
+      lang: 'fr',
+      submitText: 'Submit',
+    },
+  ].forEach(({ lang, submitText }) => {
+    test(`language ${lang} should have ${submitText} as button text`, async ({ page }) => {
+      await page.goto(`/?lang=${lang}`);
+      await expect(page.locator('button[type=submit]')).toHaveText(submitText);
+    });
   });
 });
