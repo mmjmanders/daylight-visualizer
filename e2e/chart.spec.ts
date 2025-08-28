e19c545da172e2dd90c4a45a9f06f797f8a876c1
Mark Manders <mmjmanders@gmail.com>
2025-08-28 13:00:00 +0200

    Skip test on small screens

--- e2e/chart.spec.ts
+++ e2e/chart.spec.ts
@@ -13,7 +13,8 @@ test('show chart on valid data', async ({ page }) => {
   await expect(page.locator('div.highcharts-container')).toBeVisible();
 });
 
-test('should switch chart type and css class', async ({ page }) => {
+test('should switch chart type and css class', async ({ page, viewport }) => {
+  test.skip(viewport != null && viewport.width < 992, 'Test only runs on wide screens');
   await page.goto('/');
   await page.locator('button[type=button]').click();
   await page.locator('button[type=submit]').click();
