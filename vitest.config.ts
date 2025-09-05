ddaf38f0a8125f6189b231162f26130793612a62
Mark Manders <mmjmanders@gmail.com>
2025-09-05 10:42:22 +0200

    Added unit test coverage and test results

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,14 +1,21 @@
-import { fileURLToPath } from 'node:url'
-import { mergeConfig, defineConfig, configDefaults } from 'vitest/config'
-import viteConfig from './vite.config'
+import { fileURLToPath } from 'node:url';
+import { mergeConfig, defineConfig, configDefaults } from 'vitest/config';
+import viteConfig from './vite.config';
 
 export default mergeConfig(
   viteConfig,
   defineConfig({
     test: {
+      outputFile: { html: 'vitest-report/index.html' },
+      reporters: ['html'],
+      coverage: {
+        extension: ['.vue'],
+        provider: 'v8',
+        reporter: ['text', 'html'],
+      },
       environment: 'happy-dom',
       exclude: [...configDefaults.exclude, 'e2e/**'],
       root: fileURLToPath(new URL('./', import.meta.url)),
     },
   }),
-)
+);
