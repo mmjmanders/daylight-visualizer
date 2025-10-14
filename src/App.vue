d4c4c5a59a7ea5156c2ed721c1673ae467faf9a4
Mark Manders <mmjmanders@gmail.com>
2025-10-14 12:10:12 +0200

    Fixed header (#61)

--- src/App.vue
+++ src/App.vue
@@ -8,17 +8,14 @@ const { VITE_BUILD_SHA: buildSha, VITE_APP_VERSION: version } = import.meta.env;
 </script>
 
 <template>
-  <header class="d-flex justify-content-center mb-4">
-    <h1 class="position-relative">
-      <FontAwesomeIcon :icon="faSun" />
-      Daylight Visualizer
-      <span class="position-absolute font-monospace"
-        >v{{ version }}-{{ buildSha }} (Powered by
-        <a class="text-body" href="https://sunrisesunset.io/" rel="noopener" target="_blank"
-          >SunriseSunset.io</a
-        >)</span
-      >
-    </h1>
+  <header class="d-flex flex-column align-items-center mb-4">
+    <h1><FontAwesomeIcon :icon="faSun" /> Daylight Visualizer</h1>
+    <h6 class="font-monospace fw-normal">
+      v{{ version }}-{{ buildSha }} (Powered by
+      <a class="text-body" href="https://sunrisesunset.io/" rel="noopener" target="_blank"
+        >SunriseSunset.io</a
+      >)
+    </h6>
   </header>
   <main class="d-flex flex-column gap-2">
     <MainContent />
@@ -27,9 +24,11 @@ const { VITE_BUILD_SHA: buildSha, VITE_APP_VERSION: version } = import.meta.env;
 </template>
 
 <style scoped>
-span {
+h1,
+h6 {
+  margin-bottom: 0;
+}
+h6 {
   font-size: 0.75rem;
-  right: 0;
-  bottom: -0.75rem;
 }
 </style>
