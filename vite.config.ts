d550be1d5c2103e9ce52d193ec1bdeeaa9677d54
Mark Manders <mmjmanders@gmail.com>
2025-12-24 15:38:05 +0100

    Integrate PWA support using `vite-plugin-pwa` and remove unused web m… (#119)
    * Integrate PWA support using `vite-plugin-pwa` and remove unused web manifest.
    
    * Restrict auto-merge to Dependabot only in CI workflow

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,45 @@ import { fileURLToPath, URL } from 'node:url';
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import vueDevTools from 'vite-plugin-vue-devtools';
+import { VitePWA } from 'vite-plugin-pwa';
+
+const { REPO_NAME } = process.env;
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueDevTools()],
+  plugins: [
+    vue(),
+    vueDevTools(),
+    VitePWA({
+      devOptions: {
+        enabled: true,
+      },
+      registerType: 'autoUpdate',
+      strategies: 'generateSW',
+      manifest: {
+        name: 'Daylight Visualizer',
+        short_name: 'Daylight Visualizer',
+        icons: [
+          {
+            src: 'web-app-manifest-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+            purpose: ['any'],
+          },
+          {
+            src: 'web-app-manifest-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: ['any'],
+          },
+        ],
+        theme_color: '#ffffff',
+        background_color: '#ffffff',
+        display: 'standalone',
+      },
+    }),
+  ],
+  base: (REPO_NAME && `/${REPO_NAME}/`) || '/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
